35786aa62f6bf4f8ad036e407989a4da9858a18b
Deo <deolanxe@gmail.com>
2024-10-25 12:53:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const display = document.getElementById("display");
+const alphabet = ["E","R","O"]
+let calculated = false
+
+function appendToDisplay(input){
+    let found = false
+    for (const item of alphabet) {
+        if (display.value.includes(item)) {
+          found = true;
+          break;
+        }
+      }
+    if (found == true){
+        display.value = ""
+    }
+    if (calculated == true){
+        display.value = ""
+        calculated = false
+    }
+    display.value += input;
+}
+
+function clearScreen(){
+    display.value = ""
+}
+
+function calc(){
+    try{
+        display.value =  eval(display.value)
+        calculated = true
+    }
+    catch(error){
+        display.value = "ERROR"
+    }
+}
