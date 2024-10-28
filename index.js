cb0228c7a5891c0f7906020259950ae6207f11eb
Deo <deolanxe@gmail.com>
2024-10-28 08:42:00 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const display = document.getElementById("display");
 const alphabet = ["E","R","O"]
 let calculated = false
 
+display.focus;
+display.setSelectionRange(display.value.length,display.value.length);
+
 function appendToDisplay(input){
     let found = false
     for (const item of alphabet) {
